59f6447f9bfc98ab16cb450f1640c1613ffa7bc2
Thameem Abbas <tabbas97@gmail.com>
2024-03-29 00:51:15 -0400

    Add: First Valid Streamlit app

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
 import streamlit as st
 from data.local_chroma_connection import LocalChromaConnection
-from data.embeddings import EmbeddingsGenerator
-import arxiv_retrieve
+import chroma_wrapper
+import json
 
-def run_search(input_query: str):
-    coll = LocalChromaConnection.get_collection('arxiv-research-paper')
-    search_results = coll.query(
-        query_embeddings=EmbeddingsGenerator.get_embeddings([input_query]),
-        n_results=5
-    )
+searcher = chroma_wrapper.ChromaSearchWrapper('arxiv-research-paper')
+
+# st.session_state["results"] = st.container()
+
+def format_authors(authors):
+    authors = json.loads(authors)
+    print(authors)
+    out = ",".join([" ".join(author) for author in authors])
+    return out
+
+def run_search():
     
-    # Get the respective elements from streamlit and update them
+    res_container : st = st.session_state["results"]
+    
+    query = st.session_state.get('query')
+    query_results = searcher.run_query(query)
+    reranked_results = searcher.rerank_queries(query, query_results)
+    for result in reranked_results:
+        print(result.keys())
+        print(result['meta'].keys())
+        with res_container.expander(result['meta']['title']):
+            text_tab, meta_tab = st.tabs(["Abstract", "Metadata"])
+            with meta_tab:
+                st.write("ID : ", result['meta']['id'])
+                st.write("Link : ", "https://arxiv.org/abs/" + result['meta']['id'])
+                st.write("Authors : ", format_authors(result['meta']['authors']))
+                st.write("Last Updated", result['meta']['last_updated'])
+                st.write("License", result['meta']['license'])
+            with text_tab:
+                st.write(result['text'])
+                
+    st.session_state["results"] = res_container        
+    
 
 st.title('QuickResearch')
-st.text_input('Enter your research topic here:')
+user_input = st.text_input('Enter your research topic here:')
 
-st.text('Results will appear here')
+if 'query' not in st.session_state:
+    st.session_state.query = ""
+    
+st.session_state.query = user_input
 
+st.button(
+    'Search',
+    on_click=run_search
+    )
 
-
-
-
-
-
+st.text('Results will appear below:')
+if 'results' not in st.session_state:
+    st.session_state["results"] = st.empty()
